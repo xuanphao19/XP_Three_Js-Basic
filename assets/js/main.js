5016ce0a510e96697fabf9b743d58b28b234f24b
Xuan Phao <nguyenthanhhoa075@gmail.com>
2023-10-03 14:25:03 +0700

    renderer.rende

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,4 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
