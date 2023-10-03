6d446df547cc20681605b5771780c34e38918e4a
Xuan Phao <nguyenthanhhoa075@gmail.com>
2023-10-03 14:30:26 +0700

    cdn.skypack.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
-
+import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
