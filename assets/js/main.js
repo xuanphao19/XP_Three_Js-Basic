ac524d89ee1598e09237f4abe613ebc6dbac0cae
Xuan Phao <nguyenthanhhoa075@gmail.com>
2023-10-03 14:19:27 +0700

    unpkg.com/three@

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,13 @@
 import * as THREE from 'three';
-// import starsBgr from './assert/Rocket.png';
-import starsBgr from '../img/Rocket.png';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-const renderer = new THREE.WebGLRenderer();
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-const controls = new OrbitControls(camera, renderer.domElement);
+const renderer = new THREE.WebGLRenderer();
 const loader = new GLTFLoader();
+const textureLoader = new THREE.TextureLoader();
+const controls = new OrbitControls(camera, renderer.domElement);
 
 renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
 document.body.appendChild(renderer.domElement);
@@ -17,17 +15,17 @@ camera.position.set(0, 0, 10);
 camera.lookAt(0, 0, 0);
 
 /* ============================ */
-const textureLoader = new THREE.TextureLoader();
+
+const starsBgr = textureLoader.load('./assets/img/Rocket.png');
 
 const geometry = new THREE.BoxGeometry(2, 3, 2);
 const material = new THREE.MeshBasicMaterial({
-  map: textureLoader.load([starsBgr]),
+  map: starsBgr,
 });
 const cube = new THREE.Mesh(geometry, material);
 cube.rotation.x = 0.5;
 scene.add(cube);
 
-const lineGeometry = new THREE.LineBasicMaterial({ color: 0x0000ff });
 const points = [];
 points.push(new THREE.Vector3(-5, 0, 0));
 points.push(new THREE.Vector3(0, 5, 0));
@@ -35,6 +33,7 @@ points.push(new THREE.Vector3(5, 0, 0));
 points.push(new THREE.Vector3(-5, 0, 0));
 
 const lineMaterial = new THREE.BufferGeometry().setFromPoints(points);
+const lineGeometry = new THREE.LineBasicMaterial({ color: 0xfafafa });
 
 const line = new THREE.Line(lineMaterial, lineGeometry);
 scene.add(line);
